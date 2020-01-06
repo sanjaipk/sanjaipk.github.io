b110f0d8f871ded0c97aad6326934a96f0eedec5
sanjai <sanjai.com@gmail.com>
2020-01-06 03:26:37 -0500

    5Jan2020-Release-BaseSite

---
+++ precache-manifest.dd4188962ff7868a5d6a55605a3626df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "866184bf2bb85621a389364aa54c5b6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9eeb56ec2f08f48c0bb3",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "c0f7ba949b1752e0af84",
+    "url": "/static/css/main.701fb292.chunk.css"
+  },
+  {
+    "revision": "9eeb56ec2f08f48c0bb3",
+    "url": "/static/js/2.13cece30.chunk.js"
+  },
+  {
+    "revision": "e6edf5f5109ad7460f75a9f5716d868f",
+    "url": "/static/js/2.13cece30.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c0f7ba949b1752e0af84",
+    "url": "/static/js/main.7bfc3146.chunk.js"
+  },
+  {
+    "revision": "871858e0eabf971b8d25",
+    "url": "/static/js/runtime-main.fbb7cdf0.js"
+  }
+]);

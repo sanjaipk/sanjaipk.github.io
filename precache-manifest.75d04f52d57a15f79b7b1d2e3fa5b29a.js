da84bfb30a6be5bce5f580669dafeb409499a078
sanjai <sanjai.com@gmail.com>
2020-06-15 03:00:14 -0400

    push new websote with blog

---
+++ precache-manifest.75d04f52d57a15f79b7b1d2e3fa5b29a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65e739c4515999665c81b9823e5c06fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e96a8bf0eb012287e151",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "3fd2a79dc49938d324fa",
+    "url": "/static/css/main.701fb292.chunk.css"
+  },
+  {
+    "revision": "e96a8bf0eb012287e151",
+    "url": "/static/js/2.a6d7ed42.chunk.js"
+  },
+  {
+    "revision": "e6edf5f5109ad7460f75a9f5716d868f",
+    "url": "/static/js/2.a6d7ed42.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3fd2a79dc49938d324fa",
+    "url": "/static/js/main.b52041fb.chunk.js"
+  },
+  {
+    "revision": "871858e0eabf971b8d25",
+    "url": "/static/js/runtime-main.fbb7cdf0.js"
+  }
+]);
